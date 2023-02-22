47390829d704e634886fe70bdb5a00925d64a00e
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2023-02-23 03:54:31 +0700

    update hardware endpoint

--- main_local.py
+++ main_local.py
@@ -17,6 +17,7 @@ mysql = MySQL(app)
    
 default_value = "null"
 
+#login user CLEAR
 @app.route('/users/login/',methods=['POST'])
 def login_user():
 
@@ -35,7 +36,8 @@ def login_user():
         user = cur.fetchall()
 
         return jsonify({"error": False,"message": "login success!", "loginResult":{"idUser":user[0][0],"email":user[0][1],"username":user[0][2],"password":user[0][3]}})
-
+    
+#login user UNCLEAR, respon belum benar
 @app.route('/users/register/',methods=['POST'])
 def register_user():
 
@@ -73,50 +75,58 @@ def get_a_user_by_username():
 
     return jsonify(data)
 
+# save hardware to db CLEAR, not duplicate anymore
 @app.route('/mokura/register',methods=['POST'])
 def register_mokura():
+    response = ()
 
-    hardware = request.form.get('hardware', default_value)
+    hardware_name = request.form.get('hardware_name', default_value)
+    hardware_serial = request.form.get('hardware_serial', default_value)
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO mokura(hardware_serial) VALUES(%s) WHERE NOT EXISTS (SELECT * FROM mokura WHERE hardware_serial = %s)",(hardware,hardware))
-    mysql.connection.commit()
+
+    cur.execute("SELECT * FROM mokura WHERE hardware_serial = %s AND hardware_name = %s ",(hardware_serial,hardware_name))
+    record = cur.fetchall()
+
+    if len(record) > 0:
+        cur.execute("SELECT * FROM mokura WHERE hardware_serial = %s ",(hardware_serial,))
+        response = cur.fetchall()
+    else:
+        cur.execute("INSERT IGNORE INTO mokura (hardware_serial,hardware_name) VALUES(%s,%s)",(hardware_serial,hardware_name))
+        mysql.connection.commit()
+        cur.execute("SELECT * FROM mokura WHERE hardware_serial = %s AND hardware_name = %s ",(hardware_serial,hardware_name))
+        response = cur.fetchall()
+
     cur.close()
 
-    return jsonify({"error": "false", "message": "hardware registered!"})
+    return jsonify({"error": "false","id_hardware":response[0][0],"hardware_serial":response[0][1],"message": "hardware registered!"})
 
+# get all hardware from db UNCLEAR, kurang return banyak data
 @app.route('/mokura',methods=['GET'])
 def get_all_mokura():
-    hardware_name = request.form.get('hardware_name', default_value,type=str)
-    
+    response = ()
+
+    hardware_name = request.form.get('hardware_name', default_value)
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM mokura WHERE hardware_name = %s",(hardware_name))
-    data = cur.fetchall()
-  
-    return jsonify(data)
 
-@app.route('/mokuras/',methods=['GET'])
-def get_mokura_by_user_email():
-
-    email = request.form.get('email', default_value)
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM mokuras WHERE email = %s",(email))
-    data = cur.fetchall()
-
-    if(data[0][0]==0):
-        fail_response= jsonify({"error": True, "message": "email not found"})
-        return fail_response
+    if hardware_name != "null" :
+        cur.execute("SELECT * FROM mokura WHERE hardware_name = %s",(hardware_name,))
+        response = cur.fetchall()
     else:
-        cur.execute("SELECT * FROM mokuras WHERE email = %s",(email))
-        data = cur.fetchall()
+        cur.execute("SELECT * FROM mokura")
+        response = cur.fetchall()
 
-        return jsonify({"error": False,"message": "success", "loginResult":{"data":data[0][0]}})
+    # return jsonify({"error": "false","id_hardware":response[0][0],"hardware_serial":response[0][1],"hardware_name": response[0][2]})
 
-@app.route('/datalist/',methods=['POST'])
+    return jsonify (data)
+
+@app.route('/logging/datalist/',methods=['POST'])
 def insert_datalist():
     datas = request.get_json()
     
     # parsing data list
     for data in datas:
+        id_user = data["id_user"]
+        id_hardware = data["id_hardware"]
         time_stamp = data["timeStamp"]
         lat = data["lat"]
         lon = data["lon"]
@@ -127,7 +137,7 @@ def insert_datalist():
         duty_cycle = data["dutyCycle"]   
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO logging (time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle))
+        cur.execute("INSERT INTO logging (time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) ",(time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle))
         mysql.connection.commit()
         cur.close()
 
@@ -136,3 +146,4 @@ def insert_datalist():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969,debug=True)
 
+# commit dipake ketika modify table seperti insert
