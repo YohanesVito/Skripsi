5905a44ce47ec21d2f1ed713299c10b9b33510f7
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2023-02-24 06:40:11 +0700

    api v1

--- main_local.py
+++ main_local.py
@@ -37,7 +37,7 @@ def login_user():
 
         return jsonify({"error": False,"message": "login success!", "loginResult":{"idUser":user[0][0],"email":user[0][1],"username":user[0][2],"password":user[0][3]}})
     
-#login user UNCLEAR, respon belum benar
+#register user CLEAR
 @app.route('/users/register/',methods=['POST'])
 def register_user():
     response = ()
@@ -46,13 +46,13 @@ def register_user():
     password = request.form.get('password', default_value)
     cur = mysql.connection.cursor()
 
-    cur.execute("SELECT * FROM users WHERE email = %s",(email,))
+    cur.execute("SELECT * FROM users WHERE email = %s OR username = %s",(email,username))
     record = cur.fetchall()
     if len(record) > 0:
         #user sudah ada
         response = record
 
-        return jsonify({"error": "true", "message": "email already registered","data":{"id_user":response[0][0],"email":response[0][1],"username":response[0][2],"password":response[0][3]}})
+        return jsonify({"error": "true", "message": "email or username already registered","data":{"id_user":response[0][0],"email":response[0][1],"username":response[0][2],"password":response[0][3]}})
     else:
         cur.execute("INSERT INTO users (email,username,password) VALUES (%s,%s,%s)",(email,username,password))
         mysql.connection.commit()
@@ -61,31 +61,41 @@ def register_user():
         response = record
     cur.close()
 
-    return jsonify({"error": "false", "message": "user registered!", "data":{"id_user":response[0][0],"username":response[0][1],"email":response[0][2],"password":response[0][3]}})
+    return jsonify({"error": "false", "message": "user registered!", "data":{"id_user":response[0][0],"email":response[0][1],"username":response[0][2],"password":response[0][3]}})
 
+#get user CLEAR
 @app.route('/users/',methods=['GET'])
 def get_users():
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users")
-    data = cur.fetchall()
-  
-    return jsonify(data)
-
-@app.route('/users/',methods=['POST'])
-def get_a_user_by_username():
-
+    response = ()
+    email = request.form.get('email', default_value)
     username = request.form.get('username', default_value)
-
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE username = %s",(username,))
-    data = cur.fetchall()
 
-    cur.close()
+    if email != "null":
+        #get user by email
+        cur.execute("SELECT * FROM users WHERE email = %s",(email,))
+        response = cur.fetchall()
+        return jsonify({"error":"false","message":"success","data":{"id_user":response[0][0],"email":response[0][1],"username":response[0][2],"password":response[0][3]}})
 
-    return jsonify(data)
+    elif username != "null":
+        #get user by username
+        cur.execute("SELECT * FROM users WHERE username = %s",(username,))
+        response = cur.fetchall()
+        return jsonify({"error":"false","message":"success","data":{"id_user":response[0][0],"email":response[0][1],"username":response[0][2],"password":response[0][3]}})
+    else:
+        listuser = []
+        #get all user
+        cur.execute("SELECT * FROM users")
+        users = cur.fetchall()
+        for user in users:
+            id = user[0]
+            email = user[1]
+            username = user[2]
+            password = user[3]
+            listuser.append({"error":"false","message":"success","data":{"id_user":id,"email":email,"username":username,"password":password}})
+    return jsonify(listuser)
 
-# save hardware to db CLEAR, not duplicate anymore
+#save hardware to db CLEAR
 @app.route('/mokura/register',methods=['POST'])
 def register_mokura():
     response = ()
@@ -110,7 +120,7 @@ def register_mokura():
 
     return jsonify({"error": "false","id_hardware":response[0][0],"hardware_serial":response[0][1],"message": "hardware registered!"})
 
-# get all hardware from db UNCLEAR, kurang return banyak data
+#get all hardware from db CLEAR
 @app.route('/mokura',methods=['GET'])
 def get_all_mokura():
     response = ()
@@ -121,14 +131,22 @@ def get_all_mokura():
     if hardware_name != "null" :
         cur.execute("SELECT * FROM mokura WHERE hardware_name = %s",(hardware_name,))
         response = cur.fetchall()
+        return jsonify({"error": "false", "message":"success","data":{"id_hardware":response[0][0],"hardware_serial":response[0][1],"hardware_name": response[0][2]}})
     else:
+        listhardware = []
         cur.execute("SELECT * FROM mokura")
         response = cur.fetchall()
-
+        print(response)
+        for hardware in response:
+            id = hardware[0]
+            serial = hardware[1]
+            name = hardware[2]
+            listhardware.append({"error": "false", "message":"success","data":{"id_hardware":id,"hardware_serial":serial,"hardware_name":name}})
+        
+        return jsonify(listhardware)
     # return jsonify({"error": "false","id_hardware":response[0][0],"hardware_serial":response[0][1],"hardware_name": response[0][2]})
 
-    return jsonify(response)
-
+#logging
 @app.route('/logging/datalist/',methods=['POST'])
 def insert_datalist():
     datas = request.get_json()
@@ -137,17 +155,17 @@ def insert_datalist():
     for data in datas:
         id_user = data["id_user"]
         id_hardware = data["id_hardware"]
-        time_stamp = data["timeStamp"]
+        time_stamp = data["time_stamp"]
         lat = data["lat"]
         lon = data["lon"]
         compass = data["compass"]
         speed = data["speed"]
         rpm = data["rpm"]
         battery = data["battery"]
-        duty_cycle = data["dutyCycle"]   
+        duty_cycle = data["duty_cycle"]   
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO logging (time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) WHERE id_user = %s AND id_hardware = %s",(time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle,id_user,id_hardware))
+        cur.execute("INSERT INTO logging (time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle,id_user,id_hardware) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle,id_user,id_hardware))
         mysql.connection.commit()
         cur.close()
 
