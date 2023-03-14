a585fbea4e3e3212f04ba583e832b71636571cb5
VitoDVS-Windows <yohanes.vito@ti.ukdw.ac.id>
2023-03-14 13:23:54 +0700

    update py

--- main_local.py
+++ main_local.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 # app.config['MYSQL_DB'] = 'mokura'
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'vito'
+app.config['MYSQL_PASSWORD'] = '123'
 app.config['MYSQL_DB'] = 'mokura'
 
 mysql = MySQL(app)
