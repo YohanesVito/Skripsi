b4f3ee5b97eb4f96158f8cabed611e8f5726640a
VitoDVS-Windows <yohanes.vito@ti.ukdw.ac.id>
2023-03-14 14:53:13 +0700

    update compose

--- docker/flask/main_local.py
+++ docker/flask/main_local.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # app.config['MYSQL_PASSWORD'] = '123'
 # app.config['MYSQL_DB'] = 'mokura'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '54.87.158.218'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mokura'
